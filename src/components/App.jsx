e38611cc003953f4a943494404a5cafa514eb003
Oleg <ikslprint@gmail.com>
2024-03-18 15:59:42 +0200

    add container and fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { MainContainer } from './Container/Container';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <MainContainer>YACHT APES</MainContainer>;
 };
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,5 @@
+import { Container } from './Container.styled';
+
+export const MainContainer = ({ children }) => {
+  return <Container>{children}</Container>;
+};
